50647e5f2f35852fdfaacbd44f03c88dd66788a9
Patrik Sehnoutek <patriksehnoutek@gmail.com>
2024-08-01 10:58:33 +0200

    feat: add name2oid mapping

---
+++ attributemap/name2oid.php
@@ -0,0 +1,9 @@
+<?php
+
+
+// upn, unstructuredName, faculty mapping doesn't exist in simplesamlphp mapping arrays
+$attributemap = [
+    'upn' => 'urn:oid:1.3.6.1.4.1.311.20.2.3',
+    'unstructuredName' => 'urn:oid:1.2.840.113549.1.9.2',
+    'faculty' => 'urn:oid:2.5.4.11.1',
+];
