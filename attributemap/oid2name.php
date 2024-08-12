06bf336a4acf2092fc30f3715a00aa910b714664
Patrik Sehnoutek <patriksehnoutek@gmail.com>
2024-08-12 08:41:56 +0200

    fix: emailAddress mapping

--- attributemap/oid2name.php
+++ attributemap/oid2name.php
@@ -2,6 +2,7 @@
 
 // upn, unstructuredName, faculty mapping doesn't exist in simplesamlphp mapping arrays
 $attributemap = [
+    'urn:oid:1.2.840.113549.1.9.1' => 'emailAddress', // simplesamlphp maps this oid to email which is openid scope
     'urn:oid:1.3.6.1.4.1.311.20.2.3' => 'upn',
     'urn:oid:1.2.840.113549.1.9.2' => 'unstructuredName',
     'urn:oid:2.5.4.11.1' => 'faculty',
